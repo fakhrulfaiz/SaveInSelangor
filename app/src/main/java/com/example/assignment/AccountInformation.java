9c231c4d7b5f729167366f0bd2e099e0185250ba
Fakhrul Faiz <fakhrulfaiz201@gmail.com>
2024-01-14 14:29:28 +0800

    Fix change profile picture

--- app/src/main/java/com/example/assignment/AccountInformation.java
+++ app/src/main/java/com/example/assignment/AccountInformation.java
@@ -1,11 +1,19 @@
 package com.example.assignment;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,10 +21,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.example.assignment.dialog.AddImageDialog;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,6 +37,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.Picasso;
 
 import java.util.Objects;
 
@@ -51,6 +69,10 @@ public class AccountInformation extends Fragment {
     private FirebaseAuth auth;
     private DatabaseReference userRef;
     private String currentUserID;
+    private ActivityResultLauncher<Intent> galleryLauncher, cameraLauncher;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_PICK_IMAGE = 2;
+    private ImageView profileImageView;
 
     public AccountInformation() {
         // Required empty public constructor
@@ -81,6 +103,25 @@ public class AccountInformation extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        galleryLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        handleActivityResult(result, REQUEST_PICK_IMAGE);
+                        //    handleGalleryResult(result);
+                    }
+                });
+        cameraLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        handleActivityResult(result, REQUEST_IMAGE_CAPTURE);
+                        //   handleGalleryResult(result);
+                    }
+                });
+
     }
 
     @Override
@@ -97,6 +138,7 @@ public class AccountInformation extends Fragment {
         auth = FirebaseAuth.getInstance();
 
         // Find views by ID
+        profileImageView = view.findViewById(R.id.profileImageView);
         nameChangeEditText = view.findViewById(R.id.nameChange);
         usernameChangeEditText = view.findViewById(R.id.usernameChange);
         birthDateEditText = view.findViewById(R.id.birthDate);
@@ -107,10 +149,39 @@ public class AccountInformation extends Fragment {
         currentUserID = Objects.requireNonNull(auth.getCurrentUser()).getUid();
         userRef = FirebaseDatabase.getInstance("https://assignment-1c692-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference().child("Users");
 
+        loadUserData();
+        Button changeImageButton = view.findViewById(R.id.changeImageButton);
+        changeImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImageDialog();
+            }
+        });
+        // Set onClickListener for the button
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleConfirmButtonClick();
+            }
+        });
+    }
+    private void loadUserData() {
+
+
         userRef.child(currentUserID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (snapshot.exists()) {
+                    // Load profile image
+                    if (snapshot.hasChild("profileimage")) {
+                        String image = Objects.requireNonNull(snapshot.child("profileimage").getValue()).toString();
+                        Log.d("ProfileImageURL", image);
+                        Picasso.get().load(image).placeholder(R.drawable.profile_img_loading)
+                                .memoryPolicy(MemoryPolicy.NO_CACHE).into(profileImageView);
+                    } else {
+                        profileImageView.setImageResource(R.drawable.profile_default);
+                    }
+
                     String name = snapshot.child("name").getValue(String.class);
                     String username = snapshot.child("username").getValue(String.class);
                     String birthDate = snapshot.child("birthdate").getValue(String.class);
@@ -130,25 +201,17 @@ public class AccountInformation extends Fragment {
                     if (birthDate != null) {
                         birthDateEditText.setHint(birthDate);
                     }
-                } else {
-                    Log.d("FirebaseData", "Snapshot does not exist");
+
+
                 }
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.e("FirebaseError", "Error reading data from Firebase", error.toException());
-            }
-        });
-        // Set onClickListener for the button
-        confirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                handleConfirmButtonClick();
+            public void onCancelled(DatabaseError error) {
+                // Handle onCancelled event
             }
         });
     }
-
     private void handleConfirmButtonClick() {
         // Retrieve values from EditTexts
         String name = nameChangeEditText.getText().toString();
@@ -191,7 +254,90 @@ public class AccountInformation extends Fragment {
             }
         });
     }
+    private void openImageDialog() {
+        AddImageDialog customDialog = new AddImageDialog(requireContext());
 
+        customDialog.setOnCameraClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customDialog.dismiss();
+                openCamera();
+            }
+        });
+        customDialog.setOnGalleryClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customDialog.dismiss();
+                openGallery();
+            }
+        });
+
+        customDialog.setOnCancelClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customDialog.dismiss();
+            }
+        });
+
+        customDialog.show();
+    }
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        galleryLauncher.launch(intent);
+    }
+    private void openCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Activity activity = getActivity();
+        assert activity != null;
+        if (takePictureIntent.resolveActivity(activity.getPackageManager()) != null) {
+            cameraLauncher.launch(takePictureIntent);
+        }
+    }
+    private void handleActivityResult(ActivityResult result, int requestCode) {
+        if (result.getResultCode() == Activity.RESULT_OK) {
+            Intent data = result.getData();
+            if (data != null) {
+                if (requestCode == REQUEST_IMAGE_CAPTURE || requestCode == REQUEST_PICK_IMAGE) {
+                    // Handle the captured image from the camera
+                    Uri imageUri = data.getData();
+                    DatabaseReference newUserRef = userRef.child(currentUserID);
+                    StorageReference userProfileImageRef = FirebaseStorage.getInstance().getReference().child("profile Images");
+                    StorageReference filePath = userProfileImageRef.child(currentUserID + ".jpg");
+
+                    assert imageUri != null;
+                    filePath.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(getContext(), "Profile picture changed", Toast.LENGTH_SHORT).show();
+
+                                filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        final String downloadURL = uri.toString();
+                                        newUserRef.child("profileimage").setValue(downloadURL).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+                                                Toast.makeText(getContext(), "Task Successful!", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Toast.makeText(getContext(), "Task Failed!", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                            else{
+                                Toast.makeText(getContext(), "Task Failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    }
     private void showConfirmationDialog(final String name, final String username, final String birthDate, final String email) {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
