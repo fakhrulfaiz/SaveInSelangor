e1cf4163167cfb2903fbd861fd50de5d65f151ad
IKRAMJAAFAR <116999695+IKRAMJAAFAR@users.noreply.github.com>
2024-01-18 11:39:13 +0800

    Update RightsFragment.java.java

--- app/src/main/java/com/example/assignment/RightsFragment.java
+++ app/src/main/java/com/example/assignment/RightsFragment.java
@@ -76,7 +76,7 @@ public class RightsFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+         super.onViewCreated(view, savedInstanceState);
 
         ListView listView = view.findViewById(R.id.listview);
         List<RightData> rightDataList = RightReader.readRightData(requireContext());
